5485fb988a9bea71f34b3a105b9b2d0a477bc31b
KlydeTopino <69940316+KlydeTopino@users.noreply.github.com>
2022-02-07 08:16:15 +0800

    New Unity File with Tilemaps

---
+++ GAMPRG3_2DHellOnEarth/Assets/Scripts/Player Movement/PlayerScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float MoveSpeed = 1f;
+    public float CollisionOffset = 0.05f;
+
+    public ContactFilter2D MovementFilter;
+    
+    Vector2 MovementInput;
+    Rigidbody2D rb;
+
+    List<RaycastHit2D> CastCollision = new List<RaycastHit2D>();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(MovementInput != Vector2.zero)
+        {
+            bool success = TryMove(MovementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(MovementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(MovementInput.y, 0));
+                }
+            }
+        }
+        
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(direction, MovementFilter, CastCollision, MoveSpeed * Time.fixedDeltaTime + CollisionOffset);
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * MoveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    void OnMove(InputValue MovementValue)
+    {
+        MovementInput = MovementValue.Get<Vector2>();
+    }
+}
